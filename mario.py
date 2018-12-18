a590cca0c97176b508a9a40a4fc0e462b6ab70f5
leoMurtha <aka.murtha@gmail.com>
2018-12-18 13:26:53 -0200

    stuff

--- mario.py
+++ mario.py
@@ -5,60 +5,75 @@ from cv2 import cv2
 import utils
 import os
 import neat
+import glob
 import visualize
 import logging
 from pickle import dump, load, HIGHEST_PROTOCOL
 # OpenAI Gym Imports
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
+#from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT[1:])
+env = BinarySpaceToDiscreteSpaceEnv(env, [['right'], ['right', 'A'],['A']])
+
+#print(SIMPLE_MOVEMENT)
 
 STEPS = 0
 GENERATIONS = 0
-EVALUATE_STEPS = 3000
+
 
 def evaluate(genome_path, config_path):
     genome = load(open(genome_path, 'rb'))
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
-    
-    
+
     net = neat.nn.FeedForwardNetwork.create(genome, config)
 
     env.reset()
+    #s = 0
+    #S = 400
+    state, reward, done, info = env.step(1)
     while True:
-        state, reward, done, info = env.step(env.action_space.sample())
-
-        last_score = info['score']
-        time_left = info['time']
-        #INPUT = [x_pos, small, tall, fireball, goomba_dist, koopa_l_dist, koopa_r_dist, shell_dist, piranha_open]
-        stimulus = [info['x_pos'], float(info['status'] == 'small'), float(
-            info['status'] == 'tall'), float(info['status'] == 'fireball')]
-
+        if done:
+            env.reset()
+        
         # Fixes:TypeError: Layout of the output array img is incompatible with cv::Mat (step[ndims-1] != elemsize or step[1] != elemsize*nchannels)
         state = np.ascontiguousarray(state, dtype=np.uint8)
 
-        matches = utils.enemy_matches(state)
-        distances = [1 if point and (abs(info['x_pos'] - point[0]) < 10) else 0 for point in matches[:4]]
-        #piranha_status = 1 if matches[4] else 0
+        matches, mario_pos, pit_x = utils.template_matches(state)
+        
+        distances = [utils.euclidean_weight(
+            mario_pos, match, 1, 0.15) if match and mario_pos else 0 for match in matches]
 
-        stimulus.extend(distances)
-        #stimulus.append(piranha_status)
-
+        stimulus = distances
+        
+        # Finding pit distance if there is a pit else pit_distance = 0
+        pit_distance = 0
+        if pit_x and mario_pos:
+            pit_distance = pit_x - mario_pos[0]
+        #print(pit_distance, distances)
+        
+        stimulus.append(pit_distance)
+        
         output = net.activate(stimulus)
         action = np.argmax(output)
 
         state, reward, done, info = env.step(action)
 
-        state_r = cv2.resize(cv2.cvtColor(state,cv2.COLOR_BGR2RGB), (800,600))
-
+        state = np.ascontiguousarray(state, dtype=np.uint8)
+    
+        
+        state_r = (cv2.cvtColor(
+            state, cv2.COLOR_BGR2RGB))
+        
         cv2.imshow('evaluation', state_r)
+        
+        
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             return True
+
 
 def selection(genome, config):
     """ Runs a game for each genome and calculate its fitness function
@@ -88,67 +103,84 @@ def selection(genome, config):
     time_left = 0.0
 
     env.reset()
+    state, reward, done, info = env.step(1)
     for step in range(STEPS):
-        state, reward, done, info = env.step(env.action_space.sample())
+        if done:
+            env.reset()
 
         last_score = info['score']
         time_left = info['time']
-        #INPUT = [x_pos, small, tall, fireball, goomba_dist, koopa_l_dist, koopa_r_dist, shell_dist, piranha_open]
-        stimulus = [info['x_pos'], float(info['status'] == 'small'), float(
-            info['status'] == 'tall'), float(info['status'] == 'fireball')]
+        
+        matches, mario_pos, pit_x = utils.template_matches(state)
+        
+        distances = [utils.euclidean_weight(
+            mario_pos, match, 1, 0.15) if match and mario_pos else 0 for match in matches]
 
-        # Fixes:TypeError: Layout of the output array img is incompatible with cv::Mat (step[ndims-1] != elemsize or step[1] != elemsize*nchannels)
-        state = np.ascontiguousarray(state, dtype=np.uint8)
+        stimulus = distances
+        
+        # Finding pit distance if there is a pit else pit_distance = 0
+        pit_distance = 0
+        if pit_x and mario_pos:
+            pit_distance = pit_x - mario_pos[0]
+        
+        # Jumped the hole
+        if pit_distance <= -30 and pit_distance >= -33:
+            fitness += 1000
 
-        matches = utils.enemy_matches(state)
-
-        distances = [1 if point and (abs(info['x_pos'] - point[0]) < 10) else 0 for point in matches[:4]]
-        #piranha_status = 1 if matches[4] else 0
-
-        stimulus.extend(distances)
-        #stimulus.append(piranha_status)
-
+        stimulus.append(pit_distance)
+        
         output = net.activate(stimulus)
         action = np.argmax(output)
 
         state, reward, done, info = env.step(action)
 
+        if done:
+            fitness -= 500
+            break
+
         if info['flag_get']:
-            fitness += last_score*0.1 + time_left*0.1
-        
+            fitness += 10000 + last_score*0.1 + time_left*0.1
+
         fitness += reward
 
     return fitness
 
 
 def run(config_path, nproc=4):
-    print('[STARTING EVOLUTION] GENERATIONS %s STEPS %s' % (GENERATIONS, STEPS))
+    print('[STARTING EVOLUTION] GENERATIONS %s STEPS %s' %
+          (GENERATIONS, STEPS))
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
-    
+
     # Create the population, which is the top-level object for a NEAT run.
-    population = neat.Population(config)
+    #population = neat.Population(config)
+    latest_model = max(glob.iglob('neat-checkpoint*'), key=os.path.getctime)
+
+    print(latest_model)
+    population = neat.Checkpointer.restore_checkpoint(latest_model)
 
     # Add a stdout reporter to show progress in the terminal.
     population.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     population.add_reporter(stats)
     # Saves a checkpoint every 10 generations
-    #population.add_reporter(neat.Checkpointer(10))
+    population.add_reporter(neat.Checkpointer(5))
 
     # Run for up to N = GENERATIONS generations.
     parallel_eval = neat.ParallelEvaluator(nproc, selection)
+
     best_genome = population.run(parallel_eval.evaluate, GENERATIONS)
 
     # Display the winning genome.
-    # print('\nBest genome:\n{!s}'.format(best_genome))
-    
+    print('\nBest genome:\n{!s}'.format(best_genome))
     # Saving best genome
-    print('Saving best genome to genomes/%s' % ('best_genome_g%s_s%s.pkl' % (GENERATIONS, STEPS)))
-    dump(best_genome, open('genomes/best_genome_g%s_s%s.pkl' % (GENERATIONS, STEPS), mode='wb'), protocol=HIGHEST_PROTOCOL)
-    
+    print('Saving best genome to genomes/%s' %
+          ('best_genome_g%s_s%s.pkl' % (population.generation, STEPS)))
+    dump(best_genome, open('genomes/best_genome_g%s_s%s.pkl' %
+                           (population.generation, STEPS), mode='wb'), protocol=HIGHEST_PROTOCOL)
+
     """
     #node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
     visualize.draw_net(config, best_genome, True, filename='mario.gv')
@@ -158,12 +190,13 @@ def run(config_path, nproc=4):
     #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
     #p.run(eval_genomes, 10)
 
+
 if __name__ == '__main__':
     # Determine path to configuration file. This path manipulation is
     # here so that the script will run successfully regardless of the
     # current working directory.
     parser = argparse.ArgumentParser()
-    
+
     subparsers = parser.add_subparsers(help='sub-command help', dest='command')
 
     train = subparsers.add_parser('train', help='train help')
@@ -175,21 +208,20 @@ if __name__ == '__main__':
         '-s', '--steps', type=int, help='Number of steps of the mario gym env', required=True)
     train.add_argument(
         '-p', '--nproc', type=int, help='Number of processors', default=4)
-        
 
     evaluate_parser = subparsers.add_parser('evaluate', help='evaluate help')
     evaluate_parser.add_argument(
         '-f', '--file', help='NEATs configuration filename', required=True)
     evaluate_parser.add_argument(
         '-g', '--genome', help='Genome.pkl filename', required=True)
-    
+
     args = parser.parse_args()
 
     # Common to both commands
     config_file = args.file
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir + 'config', config_file)
-    
+
     # Command selection
     if args.command == 'train':
         GENERATIONS = args.ngens
@@ -197,6 +229,6 @@ if __name__ == '__main__':
         run(config_path, args.nproc)
     elif args.command == 'evaluate':
         genome_path = os.path.join(local_dir + 'genomes', args.genome)
-        evaluate(genome_path , config_path)
-    
+        evaluate(genome_path, config_path)
+
     env.close()
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import numpy as np
 from cv2 import cv2
+import time
+from math import sqrt, pow
 
 enemies_path = 'sprites/enemies/'
+misc_path = 'sprites/misc/'
+mario_path = 'sprites/mario/'
 
 
 def load_gray_image(path):
@@ -9,20 +13,36 @@ def load_gray_image(path):
 
     return image
 
-enemies = {'goomba': load_gray_image(enemies_path + 'goomba-template.png'),
-               'koopa_left': load_gray_image(enemies_path + 'koopa-l-template.png'),
-               'koopa_right': load_gray_image(enemies_path + 'koopa-r-template.png'),
-               'shell': load_gray_image(enemies_path + 'shell-template.png'),
-               'piranha_open': load_gray_image(enemies_path + 'piranha-open.png'),}
-               #'piranha_closed': load_gray_image(enemies_path + 'piranha-closed.png'), }
-    
+
+templates = {'goomba': load_gray_image(enemies_path + 'goomba-template.png'),
+             'koopa_left': load_gray_image(enemies_path + 'koopa-l-template.png'),
+             'koopa_right': load_gray_image(enemies_path + 'koopa-r-template.png'),
+             'shell': load_gray_image(enemies_path + 'shell-template.png'),
+             'pipe-up': load_gray_image(misc_path + 'pipe-up-f.png'), }
+# 'piranha_open': load_gray_image(enemies_path + 'piranha-open.png'),}
+# 'piranha_closed': load_gray_image(enemies_path + 'piranha-closed.png'), }
+
+mario_templates = {'mario-small-left': load_gray_image(mario_path + 'mario-small-left-f.png'),
+                   'mario-small-right': load_gray_image(mario_path + 'mario-small-right-f.png'),
+                   'mario-big-left': load_gray_image(mario_path + 'mario-big-left-f.png'),
+                   'mario-big-right': load_gray_image(mario_path + 'mario-big-right-f.png')}
+
+pit_template = load_gray_image(misc_path + 'pit.png')#{'pit-left': load_gray_image(misc_path + 'pit-left.png'),
+               # 'pit-right': load_gray_image(misc_path + 'pit-right.png')}
+
+
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, [vertices], 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+
 
 def match(state, template):
-    state_gray = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+    match = cv2.matchTemplate(state, template, cv2.TM_CCOEFF_NORMED)
 
-    match = cv2.matchTemplate(state_gray, template, cv2.TM_CCOEFF_NORMED)
-
-    loc = np.where(match >= 0.85)
+    loc = np.where(match >= 0.8)
 
     # * expression unpacks lists, [::-1] gets inverting positions of y with x
     for point in zip(*loc[::-1]):
@@ -30,15 +50,42 @@ def match(state, template):
 
     return False
 
+def detect_pit(state):
+    state_r = state[210:240, 0:256]
+    pit_detected = np.where(state_r == [167])
+    
+    if pit_detected[1] != []:
+        return pit_detected[1][0]
 
-def enemy_matches(state):
-    n_enemies = len(enemies)
+    return False
 
-    enemies_matches = []
+def euclidean_weight(p1, p2, wx, wy):
+    """
+    point in R2 p1 is mario
+    """
+    distance = sqrt(pow((p1[0] - p2[0])*wx, 2) + pow((p1[1] - p2[1])*wy, 2))
+    # Object is at marios left
+    if p1[0] > p2[0]:
+        return -distance
+    return distance
 
-    for enemy_template in enemies.values():
-        point = match(state, enemy_template)
-        enemies_matches.append(point)
-        
+def template_matches(state):
+    state_gray = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
 
-    return enemies_matches
+    n_matches = len(templates)
+
+    template_matches = []
+
+    for template in templates.values():
+        point = match(state_gray, template)
+        template_matches.append(point)
+
+    mario_pos = False
+    for template in mario_templates.values():
+        mario_pos = match(state_gray, template)
+        if mario_pos:
+            break
+
+    pit_detected = detect_pit(state_gray)
+
+    return template_matches, mario_pos, pit_detected
